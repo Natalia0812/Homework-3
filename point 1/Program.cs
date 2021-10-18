e5351e061f20e2e839c0e76c4ed45c296df2a08e
Natali <Natali@DESKTOP-CRPU105>
2021-10-18 23:17:55 +0300

    Добавьте файлы проекта.

---
+++ point 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace point_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("Введите значение радиуса основания - r = ");
+            decimal r = decimal.Parse(Console.ReadLine());
+            Console.Write("Введите значение образующей - l = ");
+            decimal l = decimal.Parse(Console.ReadLine());
+
+            const decimal pi = 3.141592653M;
+            Console.WriteLine($"Значение констаты   pi={pi}");
+
+            decimal S = pi * r * (r + l);
+
+
+            Console.WriteLine($"Площадь поверхности круглогоконуса составляет {S}");
+
+
+
+
+
+           
+        }
+    }
+}
---
+++ point 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace point_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 14, y = 1, z = 5;   // sample#1
+            x += y - x++ * z;
+            Console.WriteLine($"x={x}  y={y}  z={z}   ");
+            Console.WriteLine();
+
+            x = 14; y = 1; z = 5;   // sample#2
+            z = --x - y * 5;
+            Console.WriteLine($"x={x}  y={y}  z={z}");
+            Console.WriteLine();
+
+            x = 14; y = 1; z = 5;   // sample#3
+
+            y /= x + 5 % z;
+            Console.WriteLine($"x={x}  y={y}  z={z}");
+            Console.WriteLine();
+
+            x = 14; y = 1; z = 5;   // sample#4
+            z = x++ + y * 5;
+            Console.WriteLine($"x={x}  y={y}  z={z}");
+            Console.WriteLine();
+           
+
+            x = 14; y = 1; z = 5;   // sample#5
+            x = y - x++ * z;
+            Console.WriteLine($"x={x}  y={y}  z={z}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
